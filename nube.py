3cd5c799e83879a69010a1beea6aa0d51cc1d730
JOSUEMC-088 <franciscojosue088@gmail.com>
2025-06-10 11:26:45 -0600

    nube

---
+++ nube.py
@@ -0,0 +1,22 @@
+import peewee as pw
+import pymysql
+pymysql.install_as_MySQLdb()
+
+import peewee as pw
+
+farmacia_ujat = pw.PostgresqlDatabase(
+    'neondb',
+    user='neondb_owner',
+    password='npg_rtza0QeXhy2C',
+    host='ep-winter-shape-a4p17xtq-pooler.us-east-1.aws.neon.tech',
+    port=5432,
+    sslmode='require'
+)
+
+class Receta(pw.Model):
+    medicamento = pw.CharField()
+    interacciones = pw.CharField()
+
+    class Meta:
+        database = farmacia_ujat
+        primary_key = False 
